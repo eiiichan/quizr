3efe1662699290dbd8b35b090c9d958ffb585932
Carlo Gonzales <carloagonzales@gmail.com>
2014-11-02 19:07:15 +0800

    questions server-side module initial commit

---
+++ packages/custom/questions/server/models/question.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var mongoose = requrie('mongoose');
+var Schema = mongoose.Schema;
+var questionTypes = 'multiple-choice conditional single-blank multiple-blank true/false calculation'.split(' ');
+
+var QuestionSchema = new Schema({
+	
+	question: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	questionType: {
+		type: String,
+		enum: questionTypes,
+		required: true,
+		trim: true
+	},
+	choices: [{
+		choice: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		isAnswer: {
+			type: Boolean,
+			default: false
+		}
+	}],
+
+	active: {
+		type: Boolean,
+		default: true
+	},
+	tags: [{
+		type: String,
+		trim: true
+	}],
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+
+});
+
+QuestionSchema.statics.QUESTION_TYPES = questionTypes;
+QuestionSchema.statics.load = function(id, cb){
+	this.findOne({
+		_id: id
+	}).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('Question', QuestionSchema);
--- packages/custom/questions/server/routes/questions.js
+++ packages/custom/questions/server/routes/questions.js
@@ -1,26 +1,19 @@
 'use strict';
 
+var questions = require('../controllers/questions');
+
 // The Package is past automatically as first parameter
 module.exports = function(Questions, app, auth, database) {
 
-  app.get('/questions/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
+	app.route('/questions')
+		.get(auth.requiresLogin, questions.all)
+		.post(auth.requiresLogin, questions.create);
 
-  app.get('/questions/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
+	app.route('/questions/:questionId')
+		.get(questions.show)
+		.put(auth.requiresLogin, questions.update)
+		.delete(auth.requiresLogin, questions.destroy);
 
-  app.get('/questions/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
-
-  app.get('/questions/example/render', function(req, res, next) {
-    Questions.render('index', {
-      package: 'questions'
-    }, function(err, html) {
-      //Rendering a view from the Package server/views
-      res.send(html);
-    });
-  });
+	app.param('questionId', questions.question);
+	
 };
